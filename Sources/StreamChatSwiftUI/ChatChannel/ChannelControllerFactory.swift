25f18913a2ddb225a1751f15a61e6d7cc04bc43f
Quentin <quentinmodena@protonmail.com>
2025-09-09 15:11:29 +0200

    public

--- Sources/StreamChatSwiftUI/ChatChannel/ChannelControllerFactory.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/ChannelControllerFactory.swift
@@ -10,7 +10,7 @@ public class ChannelControllerFactory {
 
     @Injected(\.chatClient) var chatClient
 
-    var currentChannelController: ChatChannelController?
+    public var currentChannelController: ChatChannelController?
     private var messageControllers = [String: ChatMessageController]()
 
     /// Creates a channel controller with the provided channel id.
