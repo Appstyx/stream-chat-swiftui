fb78d862821e76119c56fd5fc9e74ab3dd696e90
Quentin <quentinmodena@protonmail.com>
2025-09-09 15:05:05 +0200

    Add public

--- Sources/StreamChatSwiftUI/Utils.swift
+++ Sources/StreamChatSwiftUI/Utils.swift
@@ -68,7 +68,7 @@ public class Utils {
 
     var messageCachingUtils = MessageCachingUtils()
     var messageListDateUtils: MessageListDateUtils
-    var channelControllerFactory = ChannelControllerFactory()
+    public var channelControllerFactory = ChannelControllerFactory()
     
     internal var _audioPlayer: AudioPlaying?
     internal var _audioRecorder: AudioRecording?
